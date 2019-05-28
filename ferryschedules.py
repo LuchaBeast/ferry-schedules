3c8dc5d87aa2869f78695fbbf42df615e54a1b71
Drew Shamrock <drewshamrock@gmail.com>
2019-05-28 15:39:48 -0700

    Pull from google sheets

--- ferryschedules.py
+++ ferryschedules.py
@@ -4,9 +4,19 @@ from oauth2client.service_account import ServiceAccountCredentials
 
 app = Flask(__name__)
 
+scope = ['https://spreadsheets.google.com/feeds',
+                 'https://www.googleapis.com/auth/drive']
+creds = ServiceAccountCredentials.from_json_keyfile_name('client_secret.json', scope)
+client = gspread.authorize(creds)
+
+sheet = client.open_by_key('1sh4UaaL4ZVAIz4ffvYTeTo8se83rxGFaGbN4C2wjfAI')
+
+depart_seattle_am = sheet.get_worksheet(0)
+
 @app.route('/')
 def homepage():
-    return 'Hello, World!'
+    first_time = depart_seattle_am.acell('A1').value
+    return first_time
 
 if __name__ == '__main__':
     app.debug = True
