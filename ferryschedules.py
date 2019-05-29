25d559c2ed8a8c2319b6ce7c952dcc99714cb04d
Drew Shamrock <drewshamrock@gmail.com>
2019-05-29 11:28:57 -0700

    Create dictionary from range of cells and display on schedule template

--- ferryschedules.py
+++ ferryschedules.py
@@ -13,13 +13,27 @@ sheet = client.open_by_key('1sh4UaaL4ZVAIz4ffvYTeTo8se83rxGFaGbN4C2wjfAI')
 
 @app.route('/')
 def homepage():
-    schedule = sheet.get_worksheet(1).get_all_values()
-    return render_template('index.html', schedule=schedule)
+    return 'Homepage'
 
 @app.route('/bremerton-seattle/')
 def schedule():
-    schedule = sheet.get_worksheet(1).get_all_values()
-    return render_template('schedule.html', schedule=schedule)
+    schedule = sheet.get_worksheet(1)
+
+    depart_bremerton = []
+    arrive_seattle = []
+
+    depart_bremerton_cells = schedule.range('A1:A15')
+    arrive_seattle_cells = schedule.range('B1:B15')
+
+    for cell in depart_bremerton_cells:
+        depart_bremerton.append(cell.value)
+
+    for cell in arrive_seattle_cells:
+        arrive_seattle.append(cell.value)
+
+    times = dict(zip(depart_bremerton, arrive_seattle))
+
+    return render_template('schedule.html', times=times.items())
 
 if __name__ == '__main__':
     app.debug = True
