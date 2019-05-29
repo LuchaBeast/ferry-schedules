2d37675d36a8f687e8a4d7286b1e65ad9550d478
Drew Shamrock <drewshamrock@gmail.com>
2019-05-29 13:21:19 -0700

    Add return schedule code

--- ferryschedules.py
+++ ferryschedules.py
@@ -13,30 +13,68 @@ sheet = client.open_by_key('1sh4UaaL4ZVAIz4ffvYTeTo8se83rxGFaGbN4C2wjfAI')
 
 @app.route('/')
 def homepage():
-    return 'Homepage'
+    return render_template('index.html')
 
 @app.route('/bremerton-seattle/')
 def schedule():
-    schedule = sheet.get_worksheet(1)
+    # Get worksheet with schedules
+    ws = sheet.get_worksheet(1)
 
-    h1 = "Bremerton Ferry Schedule"
-    table_headers = {'Depart Bremerton':'Arrive Seattle'}
+    # Set title tag variable
+    title = ws.acell('E2').value
+    
+    # Set h1 tag variable
+    h1 = ws.acell('E3').value
 
+    # Set leadcopy variable
+    leadcopy = ws.acell('E4').value
+
+    # Set table headers for each schedule
+    table_headers_1 = {'Depart Bremerton':'Arrive Seattle'}
+    table_headers_2 = {'Depart Seattle':'Arrive Bremerton'}
+
+    # Create empty lists for storing schedule times
     depart_bremerton_schedule = []
     arrive_seattle_schedule = []
 
-    depart_bremerton_cells = schedule.range('A2:A16')
-    arrive_seattle_cells = schedule.range('B2:B16')
+    # Get the cells for each schedule
+    depart_bremerton_cells = ws.range('A2:A16')
+    arrive_seattle_cells = ws.range('B2:B16')
 
+    # Iterate through each set of cells
+    # and add the value to each list
     for cell in depart_bremerton_cells:
         depart_bremerton_schedule.append(cell.value)
 
     for cell in arrive_seattle_cells:
         arrive_seattle_schedule.append(cell.value)
 
-    times = dict(zip(depart_bremerton_schedule, arrive_seattle_schedule))
+    # Convert both lists into a single dictionary
+    times_1 = dict(zip(depart_bremerton_schedule, arrive_seattle_schedule))
 
-    return render_template('schedule.html', times=times.items(), table_headers=table_headers.items(), h1=h1)
+    # Create empty lists for storing schedule times
+    depart_seattle_schedule = []
+    arrive_bremerton_schedule = []
+
+    # Get the cells for each schedule
+    depart_seattle_cells = ws.range('A19:A33')
+    arrive_bremerton_cells = ws.range('B19:B33')
+    
+    # Iterate through each set of cells
+    # and add the value to each list
+    for cell in depart_seattle_cells:
+        depart_seattle_schedule.append(cell.value)
+
+    for cell in arrive_bremerton_cells:
+        arrive_bremerton_schedule.append(cell.value)
+
+    # Convert both lists into a single dictionary
+    times_2 = dict(zip(depart_seattle_schedule, arrive_bremerton_schedule))
+    
+
+    
+
+    return render_template('schedule.html', times_1=times_1.items(), times_2=times_2.items(), table_headers_1=table_headers_1.items(), table_headers_2=table_headers_2.items(), title=title, h1=h1, leadcopy=leadcopy)
 
 if __name__ == '__main__':
     app.debug = True
