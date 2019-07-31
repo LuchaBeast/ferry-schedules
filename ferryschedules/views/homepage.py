81ddaf01b5e3456e4a17691bffe31627861ab5a2
Drew Shamrock <drewshamrock@gmail.com>
2019-07-31 09:46:03 -0700

    Remove breadcrumb from homepage

--- ferryschedules/views/homepage.py
+++ ferryschedules/views/homepage.py
@@ -5,11 +5,13 @@ from flask import render_template
 
 @app.route('/')
 def homepage():
+    homepage = True
 
     # Retrieve all schedule links for homepage and navbar
     links = navbar.retrieve_links()
 
     return render_template('index.html',
+                            homepage=homepage,
                             ca_links=links['California'],
                             ny_links=links['New York'],
                             wa_links=links['Washington'])
