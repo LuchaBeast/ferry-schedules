ffe994df56193ac02ab1d3fa5efe84b0cd97debe
Drew Shamrock <drewshamrock@gmail.com>
2019-08-05 11:37:20 -0700

    Add caching to meta data

--- ferryschedules/models/schedule.py
+++ ferryschedules/models/schedule.py
@@ -1,29 +1,35 @@
 from ferryschedules import gsheet
+from ferryschedules import cache
 
 class Schedule:
     def __init__(self, worksheet_number):
         self.worksheet = gsheet.get_worksheet(worksheet_number)
+        self.cache_key = 'cached_meta_data_for_schedule_' + worksheet_number
 
     # Retrieve cell values from sheet for all meta data, headers and tags
     def retrieve_meta_data(self):
-        self.title_tag = self.worksheet.acell('B1').value
-        self.h1 = self.worksheet.acell('B2').value
-        self.lead_copy = self.worksheet.acell('B3').value
-        self.effective_dates = self.worksheet.acell('B4').value
-        self.h2_1 = self.worksheet.acell('B5').value
-        self.h2_2 = self.worksheet.acell('B6').value
-        self.next_departure_card_header_1 = self.worksheet.acell('B7').value
-        self.next_departure_card_header_2 = self.worksheet.acell('B8').value
-        self.md = {
-                          "Title Tag": self.title_tag,
-                          "H1": self.h1,
-                          "Lead Copy": self.lead_copy,
-                          "Effective Dates": self.effective_dates,
-                          "H2 1": self.h2_1,
-                          "H2 2": self.h2_2,
-                          "Next Departure Card Header 1": self.next_departure_card_header_1,
-                          "Next Departure Card Header 2": self.next_departure_card_header_2
-                         }
+        
+        self.md = cache.get(self.cache_key)
+        if self.md == None:
+            self.title_tag = self.worksheet.acell('B1').value
+            self.h1 = self.worksheet.acell('B2').value
+            self.lead_copy = self.worksheet.acell('B3').value
+            self.effective_dates = self.worksheet.acell('B4').value
+            self.h2_1 = self.worksheet.acell('B5').value
+            self.h2_2 = self.worksheet.acell('B6').value
+            self.next_departure_card_header_1 = self.worksheet.acell('B7').value
+            self.next_departure_card_header_2 = self.worksheet.acell('B8').value
+            self.md = {
+                            "Title Tag": self.title_tag,
+                            "H1": self.h1,
+                            "Lead Copy": self.lead_copy,
+                            "Effective Dates": self.effective_dates,
+                            "H2 1": self.h2_1,
+                            "H2 2": self.h2_2,
+                            "Next Departure Card Header 1": self.next_departure_card_header_1,
+                            "Next Departure Card Header 2": self.next_departure_card_header_2
+                            }
+            cache.set(self.cache_key, self.md)
         return self.md
 
     # Retrieve all schedule columns from the worksheet
