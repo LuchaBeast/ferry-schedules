23836116105a1f82abd32b38d65f35900089757d
Drew Shamrock <drewshamrock@gmail.com>
2019-08-05 10:20:54 -0700

    Implement caching for in Sitemap class

--- ferryschedules/models/sitemap.py
+++ ferryschedules/models/sitemap.py
@@ -3,12 +3,12 @@ from ferryschedules import cache
 
 class Sitemap:
     SITEMAP_WORKSHEET_NUMBER = 0
-    SCHEDULE_ID = 1
+    SCHEDULE_ID_COLUMN = 1
     ROOT_COLUMN = 2
     SLUG_COLUMN = 3
     ANCHOR_TEXT_COLUMN = 4
     SHORT_DESCRIPTION_COLUMN=5
-    BREADCRUMB_TEXT=6
+    BREADCRUMB_TEXT_COLUMN=6
 
     # Retrieve sitemap worksheet from google sheet
     def __init__(self):
@@ -19,18 +19,41 @@ class Sitemap:
         self.link_lists = []
 
         # Create list of lists containing all link values from the worksheet
-        self.schedule_id_column = cache.get('cached_schedule_id_column')
-        if self.schedule_id_column == None:
-            self.schedule_id_column = self.worksheet.col_values(self.SCHEDULE_ID)
-            cache.set('cached_schedule_id_column', self.schedule_id_column)
-        self.link_lists.append(self.schedule_id_column)
+        self.cached_schedule_id_column = cache.get('cached_schedule_id_column')
+        if self.cached_schedule_id_column == None:
+            self.cached_schedule_id_column = self.worksheet.col_values(self.SCHEDULE_ID_COLUMN)
+            cache.set('cached_schedule_id_column', self.cached_schedule_id_column)
+        self.link_lists.append(self.cached_schedule_id_column)
 
+        self.cached_root_column = cache.get('cached_root_column')
+        if self.cached_root_column == None:
+            self.cached_root_column = self.worksheet.col_values(self.ROOT_COLUMN)
+            cache.set('cached_root_column', self.cached_root_column)
+        self.link_lists.append(self.cached_root_column)
 
-        self.link_lists.append(self.worksheet.col_values(self.ROOT_COLUMN))
-        self.link_lists.append(self.worksheet.col_values(self.SLUG_COLUMN))
-        self.link_lists.append(self.worksheet.col_values(self.ANCHOR_TEXT_COLUMN))
-        self.link_lists.append(self.worksheet.col_values(self.SHORT_DESCRIPTION_COLUMN))
-        self.link_lists.append(self.worksheet.col_values(self.BREADCRUMB_TEXT))
+        self.cached_slug_column = cache.get('cached_slug_column')
+        if self.cached_slug_column == None:
+            self.cached_slug_column = self.worksheet.col_values(self.SLUG_COLUMN)
+            cache.set('cached_slug_column', self.cached_slug_column)
+        self.link_lists.append(self.cached_slug_column)
+
+        self.cached_anchor_text_column = cache.get('cached_anchor_text_column')
+        if self.cached_anchor_text_column == None:
+            self.cached_anchor_text_column = self.worksheet.col_values(self.ANCHOR_TEXT_COLUMN)
+            cache.set('cached_anchor_text_column', self.cached_anchor_text_column)
+        self.link_lists.append(self.cached_anchor_text_column)
+
+        self.cached_short_description_column = cache.get('cached_short_description_column')
+        if self.cached_short_description_column == None:
+            self.cached_short_description_column = self.worksheet.col_values(self.SHORT_DESCRIPTION_COLUMN)
+            cache.set('cached_short_description_column', self.cached_short_description_column)
+        self.link_lists.append(self.cached_short_description_column)
+
+        self.cached_breadcrumb_text = cache.get('cached_breadcrumb_text')
+        if self.cached_breadcrumb_text == None:
+            self.cached_breadcrumb_text = self.worksheet.col_values(self.BREADCRUMB_TEXT_COLUMN)
+            cache.set('cached_breadcrumb_text', self.cached_breadcrumb_text)
+        self.link_lists.append(self.cached_breadcrumb_text)
 
         # Transpose link list to pair up each link's info into its own list
         self.link_lists = list(map(list, zip(*self.link_lists)))
